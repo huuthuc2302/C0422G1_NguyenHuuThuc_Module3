0c0ebb8ffb040eafb3b9cbe415bc5fc11ad0fc5f
Your Name <you@example.com>
2022-08-01 23:53:06 +0700

    SS

---
+++ ss12/erexcise/update-users/src/main/java/com/codegym/repository/impl/UserRepository.java
@@ -0,0 +1,148 @@
+package com.codegym.repository.impl;
+
+import com.codegym.model.User;
+import com.codegym.repository.BaseRepository;
+import com.codegym.repository.IUserRepository;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserRepository implements IUserRepository {
+
+    private static final String INSERT_USERS_SQL = "INSERT INTO users (name, email, country) VALUES (?, ?, ?);";
+    private static final String SELECT_USER_BY_ID = "select id,name,email,country from users where id =?";
+    private static final String SELECT_ALL_USERS = "select * from users";
+    private static final String DELETE_USERS_SQL = "delete from users where id = ?;";
+    private static final String UPDATE_USERS_SQL = "update users set name = ?,email= ?, country =? where id = ?;";
+    private static final String SELECT_BY_COUNTRY = "select * from users where country like ?;";
+    BaseRepository baseRepository = new BaseRepository();
+
+    public UserRepository() {
+    }
+
+    @Override
+    public void insertUser(User user) throws SQLException {
+
+
+        try (Connection connection = baseRepository.getConnectDB();
+            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL);){
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getEmail());
+            preparedStatement.setString(3, user.getCountry());
+            System.out.println(preparedStatement);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            printSQLException(e);
+        }
+    }
+
+    private void printSQLException(SQLException ex) {
+        for (Throwable e : ex) {
+            if (e instanceof SQLException) {
+                e.printStackTrace(System.err);
+                System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+                System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+                System.err.println("Message: " + e.getMessage());
+                Throwable t = ex.getCause();
+                while (t != null) {
+                    System.out.println("Cause: " + t);
+                    t = t.getCause();
+                }
+            }
+        }
+    }
+
+    @Override
+    public User selectUser(int id) {
+        User user = null;
+        try (Connection connection = baseRepository.getConnectDB();
+             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_USER_BY_ID);) {
+            preparedStatement.setInt(1, id);
+            System.out.println(preparedStatement);
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while (rs.next()) {
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String country = rs.getString("country");
+                user = new User(id, name, email, country);
+            }
+        } catch (SQLException e) {
+            printSQLException(e);
+        }
+        return user;
+    }
+
+    @Override
+    public List<User> selectAllUsers() {
+
+        List<User> userList = new ArrayList<>();
+
+        try (Connection connection =  baseRepository.getConnectDB();
+             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_USERS);) {
+            System.out.println(preparedStatement);
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String country = rs.getString("country");
+                userList.add(new User(id, name, email, country));
+            }
+        } catch (SQLException e) {
+            printSQLException(e);
+        }
+        return userList;
+    }
+
+    @Override
+    public boolean deleteUser(int id) throws SQLException {
+        boolean rowDeleted;
+        try (Connection connection =  baseRepository.getConnectDB(); PreparedStatement statement = connection.prepareStatement(DELETE_USERS_SQL);) {
+            statement.setInt(1, id);
+            rowDeleted = statement.executeUpdate() > 0;
+        }
+        return rowDeleted;
+    }
+
+    @Override
+    public boolean updateUser(User user) throws SQLException {
+        boolean rowUpdated;
+        try (Connection connection =  baseRepository.getConnectDB(); PreparedStatement statement = connection.prepareStatement(UPDATE_USERS_SQL);) {
+            statement.setString(1, user.getName());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, user.getCountry());
+            statement.setInt(4, user.getId());
+
+            rowUpdated = statement.executeUpdate() > 0;
+        }
+        return rowUpdated;
+    }
+
+    @Override
+    public List<User> findCountry(String country) throws SQLException {
+        List<User> userList = new ArrayList<>();
+
+        try(Connection connection = baseRepository.getConnectDB();
+            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_BY_COUNTRY);) {
+
+            preparedStatement.setString(1, country);
+            System.out.println(preparedStatement);
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next()){
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                User user = new User(id, name, email,country);
+                userList.add(user);
+            }
+        }
+        catch(SQLException e){
+            printSQLException(e);
+        }
+
+        return null;
+    }
+}
---
+++ ss12/erexcise/update-users/src/main/webapp/user/list.jsp
@@ -0,0 +1,50 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ADMIN
+  Date: 8/1/2022
+  Time: 2:58 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<html>
+<head>
+    <title>User Management Application</title>
+</head>
+<body>
+<center>
+    <h1>Quản lý người dùng</h1>
+    <h2>
+        <a href="/users?action=create">Thêm người dùng mới</a>
+    </h2>
+    <form action="/users?action=find" method="post">
+        <input type="text" name="country" placeholder="Quốc gia cần tìm">
+        <button name="action" type="submit">Tìm</button>
+    </form>
+</center>
+<div align="center">
+    <table border="1" cellpadding="5">
+        <caption><h2>Danh sách người dùng</h2></caption>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Country</th>
+            <th>Actions</th>
+        </tr>
+        <c:forEach var="user" items="${listUser}">
+            <tr>
+                <td><c:out value="${user.id}"/></td>
+                <td><c:out value="${user.name}"/></td>
+                <td><c:out value="${user.email}"/></td>
+                <td><c:out value="${user.country}"/></td>
+                <td>
+                    <a href="/users?action=edit&id=${user.id}">Edit</a>
+                    <a href="/users?action=delete&id=${user.id}">Delete</a>
+                </td>
+            </tr>
+        </c:forEach>
+    </table>
+</div>
+</body>
+</html>
